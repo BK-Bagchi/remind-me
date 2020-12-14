29128d9f79e590fb051c0924408be34cc1a2cd38
BK-Bagchi <bkbagchi.dipto@gmail.com>
2020-12-14 21:41:45 +0600

    Toggle between dark and light mode makeing done

--- src/react/Todo/Todo.js
+++ src/react/Todo/Todo.js
@@ -2,16 +2,25 @@ import React, { useState } from 'react';
 import './Todo.css';
 
 const Todo = () => {
-    const bgName = ['light-bg', 'dark-bg']
+    const bgName = [
+        {
+            pic: 'light-bg-pic',
+            color: 'light-bg-color'
+        },
+        {
+            pic: 'dark-bg-pic',
+            color: 'dark-bg-color'
+        }
+    ]
     const [bgIndex, setBgIndex] = useState(0)
     const colorMode = () => (bgIndex === 1) ? setBgIndex(0) : setBgIndex(1)
 
     return (
         <section className="todo">
-            <div className={`top ${bgName[bgIndex]}`}>
+            <div className={`top ${bgName[bgIndex].pic}`}>
                 <button onClick={() => colorMode()}>Change</button>
             </div>
-            <div className="bottom">
+            <div className={`bottom ${bgName[bgIndex].color}`}>
 
             </div>
         </section>
