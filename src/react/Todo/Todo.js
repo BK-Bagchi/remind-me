3d331d759222f73f1309eee6b1da8dd249dfede6
BK-Bagchi <bkbagchi.dipto@gmail.com>
2020-12-16 16:14:33 +0600

    Styling bugs have been solved

--- src/react/Todo/Todo.js
+++ src/react/Todo/Todo.js
@@ -11,8 +11,8 @@ const Todo = () => {
     const dlt = useDispatch()
 
     const bgName = [
-        { pic: 'light-bg-pic', color: 'light-bg-color', time: 'Night', icon: faMoon, listBg: 'list-night' },
-        { pic: 'dark-bg-pic', color: 'dark-bg-color', time: 'Day', icon: faSun, listBg: 'list-day' }
+        { pic: 'light-bg-pic', color: 'light-bg-color', time: 'Night', icon: faMoon, listBg: 'list-night', calBg: 'calculation-bg-light' },
+        { pic: 'dark-bg-pic', color: 'dark-bg-color', time: 'Day', icon: faSun, listBg: 'list-day', calBg: 'calculation-bg-dark' }
     ]
     const [bgIndex, setBgIndex] = useState(0)
     const [todoWork, setTodoWork] = useState('')
@@ -38,7 +38,7 @@ const Todo = () => {
             <main className="w-100 d-flex justify-content-center">
                 <div className="d-flex flex-column align-items-center">
                     <div className="d-flex justify-content-around align-items-center w-100 my-3">
-                        <h1 className={`${bgName[bgIndex].time}`} onClick={() => colorMode()}>To Do</h1>
+                        <h1 className={`${bgName[bgIndex].time}`} onClick={() => colorMode()}>Remind Me</h1>
                         <p className={`font ${bgName[bgIndex].time}`} onClick={() => colorMode()}><FontAwesomeIcon icon={bgName[bgIndex].icon} /></p>
                         {/* <button onClick={() => colorMode()}>{bgName[bgIndex].time}</button> */}
                     </div>
@@ -46,7 +46,7 @@ const Todo = () => {
                         <input type="text" placeholder="Enter your To Do here" onBlur={settingTodo} />
                         <button onClick={addingTodo}>Add</button>
                     </div>
-                    <section className={`todo-work-list my-3 ${bgName[bgIndex].listBg}`}>
+                    <section className={`todo-work-list my-3 ${bgName[bgIndex].listBg} w-100`}>
                         {
                             todo.map((todo, i) => {
                                 return (
@@ -59,7 +59,7 @@ const Todo = () => {
                             })
                         }
                     </section>
-                    <div className={`d-flex justify-content-between ${bgName[bgIndex].time}`}>
+                    <div className={`d-flex justify-content-between ${bgName[bgIndex].calBg}`}>
                         <p>Total: {todo.length} &nbsp;</p>
                         <p>Done: {todo.filter(done => done.done === true).length}</p>
                     </div>
